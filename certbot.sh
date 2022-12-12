57aa53e2a908fedbaae73910a1d76c242e7c5260
Kurt, Cheung Mun See <me@kurtcms.org>
2022-12-12 00:32:36 -0800

    Fix bug with dirpath and certpath

--- certbot.sh
+++ certbot.sh
@@ -7,10 +7,11 @@ read -p 'Email address for registration and recovery with EFF: ' email
 [ -z $email ] && echo "Email cannot be empty" && exit 1
 
 # Generate a NGINX config with the given domain
-[ ! -f ../nginx-conf/nginx.conf-sample ] \
+dirpath=$(dirname $(dirname $0))
+[ ! -f $dirpath/nginx-conf/nginx.conf-sample ] \
     && echo "Sample NGINX config is not found" && exit 1
-cat ../nginx-conf/nginx.conf-sample | \
-    sed "s/ROOT_DOMAIN/$domain/" > ../nginx-conf/nginx.conf
+cat $dirpath/nginx-conf/nginx.conf-sample | \
+    sed "s/ROOT_DOMAIN/$domain/" > $dirpath/nginx-conf/nginx.conf
 
 if command -v docker-compose &> /dev/null; then
     docker-compose up -d
@@ -20,16 +21,16 @@ fi
 
 # Create a dummy TLS certificate and private key and 
 # restart NGINX to load the dummy certificate
-dirname = $(echo $(basename $(dirname $(pwd))) | sed "s/\.//")
-path = /var/lib/docker/volumes/${dirname}_certbot/_data/live/$domain/
-mkdir -p $path && openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
-    -keyout ${path}privkey.pem -out ${path}fullchain.pem -subj '/CN=localhost'
+dirname=$(echo $(basename $(dirname $(dirname $0))) | sed "s/\.//")
+certpath=/var/lib/docker/volumes/${dirname}_certbot/_data/live/$domain/
+mkdir -p $certpath && openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
+    -keyout ${certpath}privkey.pem -out ${certpath}fullchain.pem -subj '/CN=localhost'
 docker-compose restart nginx
 
 # Remove the dummy certificate and private key before 
 # requesting a signed TLS certificate with Certbot
 # and restart NGINX to load the new certificate
-rm -r $(dirname $path)
+rm -r $(dirname $certpath)
 docker-compose run --rm --entrypoint "certbot certonly --webroot \
     --webroot-path=/var/www/html --email $email --agree-tos --eff-email \
     --force-renewal -d $domain -d www.$domain" certbot
