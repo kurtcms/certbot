843de7872183c561ab4d0b77eabd6b9c13b83a8e
Kurt, Cheung Mun See <me@kurtcms.org>
2022-12-12 17:10:53 -0800

    Add path to Compose file to Docker Compose

--- certbot.sh
+++ certbot.sh
@@ -14,7 +14,7 @@ cat $dirpath/nginx-conf/nginx.conf-sample | \
     sed "s/ROOT_DOMAIN/$domain/" > $dirpath/nginx-conf/nginx.conf
 
 if command -v docker-compose &> /dev/null; then
-    docker-compose up -d
+    docker-compose -f $dirpath/docker-compose.yml up -d
 else
     echo "Docker Compose is not installed" && exit 1
 fi
@@ -25,15 +25,15 @@ dirname=$(echo $(basename $(dirname $(dirname $0))) | sed "s/\.//")
 certpath=/var/lib/docker/volumes/${dirname}_certbot/_data/live/$domain/
 mkdir -p $certpath && openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
     -keyout ${certpath}privkey.pem -out ${certpath}fullchain.pem -subj '/CN=localhost'
-docker-compose restart nginx
+docker-compose -f $dirpath/docker-compose.yml restart nginx
 
 # Remove the dummy certificate and private key before 
 # requesting a signed TLS certificate with Certbot
 # and restart NGINX to load the new certificate
 rm -r $(dirname $certpath)
-docker-compose run --rm --entrypoint "certbot certonly --webroot \
+docker-compose -f $dirpath/docker-compose.yml run --rm --entrypoint "certbot certonly --webroot \
     --webroot-path=/var/www/html --email $email --agree-tos --eff-email \
     --force-renewal -d $domain -d www.$domain" certbot
-docker-compose restart nginx
+docker-compose -f $dirpath/docker-compose.yml restart nginx
 
 systemctl enable docker
